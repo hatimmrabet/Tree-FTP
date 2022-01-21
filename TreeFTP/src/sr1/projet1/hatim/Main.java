eb547c6b714de7d2341c9a9ba0e5ef2eb9f0d1c3
Hatim Mrabet el khomssi <hatim.mrabetelkhomssi.etu@univ-lille.fr>
2022-01-22 00:09:28 +0100

    working version

--- TreeFTP/src/sr1/projet1/hatim/Main.java
+++ TreeFTP/src/sr1/projet1/hatim/Main.java
@@ -5,73 +5,102 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 
 public class Main {	
-	public static void main(String[] args) {
+	
+	public static void afficherContent(String path, int profendeur, PrintWriter printer,BufferedReader reader) throws IOException
+	{
+		String content;
+		ArrayList<String> files = new ArrayList<>();
+		System.out.println(path);
 		
+		//using passive mode
+		printer.println("PASV");
+		content = reader.readLine();
+		//System.out.println(content);
+		String[] pasv = content.split(" ")[4].replace("(","").replace(")","").split(",");
+		String IP = pasv[0]+"."+pasv[1]+"."+pasv[2]+"."+pasv[3];
+		int port = (Integer.parseInt(pasv[4]) * 256) + Integer.parseInt(pasv[5]);	
+		//System.out.println(IP+" "+port);
+		
+		Socket s2 = new Socket(InetAddress.getByName(IP),port);
+		BufferedReader readerdata = new BufferedReader(new InputStreamReader(s2.getInputStream()));
+			
+		printer.println("list "+path);
+		content = reader.readLine();
+		//System.out.println(content);
+		content = reader.readLine();
+		//System.out.println(content);
+		while((content = readerdata.readLine())!= null)
+		{
+			//System.out.println(content);
+			files.add(content.split("\\s+")[8]);
+		}
+		
+		for(String file : files)
+		{
+			System.out.println(file);
+			if (profendeur>1)
+			{
+				afficherContent(path+file+"/",profendeur-1,printer,reader);				
+			}
+		}
+		
+		readerdata.close();
+		s2.close();
+	}
+	
+	
+	public static void main(String[] args) {
 		try {
 			Socket s = new Socket("ftp.ubuntu.com", 21);
 			
 			OutputStream os = s.getOutputStream();
 			PrintWriter printer = new PrintWriter(os,true);
 			
-			InputStream in = s.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-			String content = reader.readLine();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			String content;
 			
+			//socket response
+			content = reader.readLine();
 			System.out.println(content);
 			
+			//user connection
 			printer.println("user anonymous");
+			content = reader.readLine();
+			System.out.println(content);
+			
 			printer.println("pass");
 			content = reader.readLine();
 			System.out.println(content);
 			
-			content = reader.readLine();
-			System.out.println(content);
 
 			printer.println("pwd");
 			content = reader.readLine();
 			System.out.println(content);
 
+			afficherContent("./",3,printer, reader);
 			
+			/*
+			//using passive mode
 			printer.println("PASV");
 			content = reader.readLine();
-			
-			String[] pasv = content.split(" ")[4].replace("(","").replace(")","").split(",");
-			
-			String IP = pasv[0]+"."+pasv[1]+"."+pasv[2]+"."+pasv[3];
-			int port = (Integer.parseInt(pasv[4]) * 256) + Integer.parseInt(pasv[5]);	
-			
-			Socket s2 = new Socket(InetAddress.getByName(IP),port);
-			
-			BufferedReader readerdata = new BufferedReader(new InputStreamReader(s2.getInputStream()));
-					
-			printer.println("CWD ./ubuntu");
-			content = reader.readLine();
 			System.out.println(content);
-			printer.println("PWD");
-			content = reader.readLine();
-			System.out.println(content);
+			pasv = content.split(" ")[4].replace("(","").replace(")","").split(",");
+			IP = pasv[0]+"."+pasv[1]+"."+pasv[2]+"."+pasv[3];
+			port = (Integer.parseInt(pasv[4]) * 256) + Integer.parseInt(pasv[5]);
+			System.out.println(IP+" "+port);
 			
-				
-			printer.println("list .");
-			
-			content = reader.readLine();
-			System.out.println(content);
-			content = reader.readLine();
-			System.out.println(content);
-			
-			while((content = readerdata.readLine())!= null)
-			{
-				System.out.println(content);
-			}
-			
-			//System.out.println(s2.isConnected());
+			s2 = new Socket(InetAddress.getByName(IP),port);
+			readerdata = new BufferedReader(new InputStreamReader(s2.getInputStream()));
+
 			
 			printer.println("list ./ubuntu");
 			content = reader.readLine();
@@ -80,10 +109,11 @@ public class Main {
 			System.out.println(content);
 			while((content = readerdata.readLine()) != null)
 			{
-				System.out.println(content.split("\\s+")[8]);
+				System.out.println(content);
 			}
-			
+			System.out.println("END DATA");
 			s2.close();
+			*/
 			s.close();
 			
 		} catch (UnknownHostException e) {
