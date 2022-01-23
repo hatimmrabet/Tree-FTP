d8492fe7f67f40603d90afa50a18e1946181e4b0
Hatim Mrabet el khomssi <hatim.mrabetelkhomssi.etu@univ-lille.fr>
2022-01-23 01:04:47 +0100

    ajout de profondeur max

--- TreeFTP/src/sr1/projet1/hatim/Main.java
+++ TreeFTP/src/sr1/projet1/hatim/Main.java
@@ -2,129 +2,116 @@ package sr1.projet1.hatim;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
+public class Main {
 
-public class Main {	
-	
-	public static void afficherContent(String path, int profendeur, PrintWriter printer,BufferedReader reader) throws IOException
-	{
+	public static void afficherContent(String path, int profondeur, int maxProfondeur, PrintWriter printer,
+			BufferedReader reader) throws IOException {
 		String content;
 		ArrayList<String> files = new ArrayList<>();
-		System.out.println(path);
-		
-		//using passive mode
+		// System.out.println(path);
+
+		// using passive mode
 		printer.println("PASV");
 		content = reader.readLine();
 		//System.out.println(content);
-		String[] pasv = content.split(" ")[4].replace("(","").replace(")","").split(",");
-		String IP = pasv[0]+"."+pasv[1]+"."+pasv[2]+"."+pasv[3];
-		int port = (Integer.parseInt(pasv[4]) * 256) + Integer.parseInt(pasv[5]);	
-		//System.out.println(IP+" "+port);
-		
-		Socket s2 = new Socket(InetAddress.getByName(IP),port);
-		BufferedReader readerdata = new BufferedReader(new InputStreamReader(s2.getInputStream()));
+
+		if (content != null) {
+			// System.out.println(content);
+			String[] pasv = content.split(" ")[4].replace("(", "").replace(")", "").split(",");
+			String IP = pasv[0] + "." + pasv[1] + "." + pasv[2] + "." + pasv[3];
+			int port = (Integer.parseInt(pasv[4]) * 256) + Integer.parseInt(pasv[5]);
+			// System.out.println(IP+" "+port);
+
+			Socket s2 = new Socket(InetAddress.getByName(IP), port);
+			BufferedReader readerdata = new BufferedReader(new InputStreamReader(s2.getInputStream()));
 			
-		printer.println("list "+path);
-		content = reader.readLine();
-		//System.out.println(content);
-		content = reader.readLine();
-		//System.out.println(content);
-		while((content = readerdata.readLine())!= null)
-		{
-			//System.out.println(content);
-			files.add(content.split("\\s+")[8]);
-		}
-		
-		for(String file : files)
-		{
-			System.out.println(file);
-			if (profendeur>1)
-			{
-				afficherContent(path+file+"/",profendeur-1,printer,reader);				
+			s2.setKeepAlive(true);
+			
+			printer.println("NLST " + path);
+			//printer.println("LIST " + path);
+			content = reader.readLine();
+			// System.out.println(content);
+			content = reader.readLine();
+			// System.out.println(content);
+			while ((content = readerdata.readLine()) != null) {
+				//System.out.println(content);
+				String[] a = content.split("/");
+				String filename = a[a.length - 1];
+				//String filename = content.split("\\s+")[8];
+				files.add(filename);
 			}
+
+			//preparer l'affichage
+			for (int i = 0; i < files.size(); i++) {
+				String display = "";
+				for (int j = 0; j < profondeur; j++) {
+					display += "\u2551 ";
+				}
+
+				if (i == files.size() - 1) {
+					display += "\u255A ";
+				} else {
+					display += "\u2560 ";
+				}
+
+				display += files.get(i);
+				System.out.println(display);
+				if (profondeur < maxProfondeur) {
+					afficherContent(path + files.get(i) + "/", profondeur + 1, maxProfondeur, printer, reader);
+				}
+			}
+			readerdata.close();
+			s2.close();
 		}
-		
-		readerdata.close();
-		s2.close();
 	}
-	
-	
-	public static void main(String[] args) {
-		try {
-			Socket s = new Socket("ftp.ubuntu.com", 21);
-			
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		/*
+		 * if (args.length < 1) {
+		 * 
+		 * } else { String adresse = args[0];
+		 */
+			Socket s = new Socket("ftp.free.fr", 21);
+			// Socket s = new Socket(adresse, 21);
+
 			OutputStream os = s.getOutputStream();
-			PrintWriter printer = new PrintWriter(os,true);
-			
+			PrintWriter printer = new PrintWriter(os, true);
+
 			BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			String content;
-			
-			//socket response
+
+			// socket response
 			content = reader.readLine();
 			System.out.println(content);
-			
-			//user connection
+
+			// user connection
 			printer.println("user anonymous");
 			content = reader.readLine();
 			System.out.println(content);
-			
+
 			printer.println("pass");
 			content = reader.readLine();
 			System.out.println(content);
-			
 
 			printer.println("pwd");
 			content = reader.readLine();
 			System.out.println(content);
 
-			afficherContent("./",3,printer, reader);
-			
-			/*
-			//using passive mode
-			printer.println("PASV");
-			content = reader.readLine();
-			System.out.println(content);
-			pasv = content.split(" ")[4].replace("(","").replace(")","").split(",");
-			IP = pasv[0]+"."+pasv[1]+"."+pasv[2]+"."+pasv[3];
-			port = (Integer.parseInt(pasv[4]) * 256) + Integer.parseInt(pasv[5]);
-			System.out.println(IP+" "+port);
-			
-			s2 = new Socket(InetAddress.getByName(IP),port);
-			readerdata = new BufferedReader(new InputStreamReader(s2.getInputStream()));
-
-			
-			printer.println("list ./ubuntu");
-			content = reader.readLine();
-			System.out.println(content);
-			content = reader.readLine();
-			System.out.println(content);
-			while((content = readerdata.readLine()) != null)
-			{
-				System.out.println(content);
-			}
-			System.out.println("END DATA");
-			s2.close();
-			*/
+			int profondeur = 3;
+			String path = "/.mirrors18/mplayerhq.hu/MPlayer/";
+			System.out.println(path);
+			afficherContent(path, 0, profondeur, printer, reader);
 			s.close();
-			
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
+//		}
+
 	}
 
 }
